e83ba306daec590d850eabf10dc06af30b99480a
sthomas <sthomas@abercrombiekent.com>
2013-12-30 19:09:16 +0000

    Added some code

--- MyGitTest/MyGitTest/Program.cs
+++ MyGitTest/MyGitTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,8 @@ namespace MyGitTest
             //
             //Some test 
             //Some for featrue 1.1
+            SoapMessage message = new SoapMessage();
+            message.Headers = new Header[Convert.ToInt32("12")];
         }
     }
 }
